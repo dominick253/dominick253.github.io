3e2eea4806f3301a23d8693f6c91540e62ce2e00
dominick253 <123394600+dominick253@users.noreply.github.com>
2023-07-06 12:30:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
-const helmet = require('helmet')
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const path = require('path');
+// const helmet = require('helmet')
+// const app = express();
+// const port = 3000;
 
-// Set the root directory
-const rootDir = path.join(__dirname, '/');
-app.use(helmet());
-// Serve static files from the root directory
-app.use(express.static(rootDir));
+// // Set the root directory
+// const rootDir = path.join(__dirname, '/');
+// app.use(helmet());
+// // Serve static files from the root directory
+// app.use(express.static(rootDir));
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-s
+// // Start the server
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
+// s
